e3ed902f1dc5193a137565922a2e84b09272e850
Juliano Antonio de Abreu Pereira <jubamarvim@yahoo.com.br>
2022-08-13 21:24:35 -0300

    Add files via upload

---
+++ quantidade_de_numeros_pares/quantidade_numeros_pares.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 13 21:15:37 2022
+
+@author: Juliano
+"""
+
+#Informar a quantidade de numeros pares menores que o numero informado pelo usuario
+
+atual = 0
+n = int(input('Informe um número: '))
+while atual < n:
+ print(atual)
+ atual += 2
